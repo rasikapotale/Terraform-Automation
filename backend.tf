b7ffdb32f3e0689d82473a155e5093b5a70eab1d
Rasika Potale <113124297+rasikapotale@users.noreply.github.com>
2025-10-30 22:57:33 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket        = "mydev-project-terraform-sample-batch-aws-devops-azure-29"
+    bucket        = "demo-app-devops-rasika-potale-batch-27-11"
     key           = "main"
     region        = "us-east-1"
     use_lockfile  = true   # ✅ new parameter
